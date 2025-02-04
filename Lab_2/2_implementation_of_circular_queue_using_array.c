e0d06d047607c3e6e132547063b9f2ee6eff8b73
Aayush Neupane <neupane0403@gmail.com>
2025-02-04 14:40:41 +0545

    circular queue using arrays

---
+++ Lab_2/2_implementation_of_circular_queue_using_array.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#define MAX 5
+struct CQueue
+{
+    int FRONT;
+    int REAR;
+    int DATA[MAX];
+};
+void Enque(struct CQueue *R, int element)
+{
+    if (R->FRONT == (R->REAR + 1) % MAX)
+    {
+        printf("QUEUE IS FULL!!!");
+    }
+    else
+    {
+        R->DATA[R->REAR] = element;
+        R->REAR = (R->REAR + 1) % MAX;
+    }
+}
+int Deque(struct CQueue *Q)
+{
+    int element;
+    if (Q->REAR == Q->FRONT)
+        printf("QUEUE IS EMPTY!!!");
+    else
+    {
+        element = Q->DATA[Q->FRONT];
+        Q->FRONT = (Q->FRONT + 1) % MAX;
+    }
+    return element;
+}
+int main()
+{
+    int choice;
+    int input;
+    struct CQueue Q = {0, 0};
+    while (1)
+    {
+        printf("Enter 1 for Enqueue and 2 for Dequeue: ");
+        scanf("%d", &choice);
+        if (choice == 1)
+        {
+            printf("Enter the value: ");
+            scanf("%d", &input);
+            Enque(&Q, input);
+        }
+        if (choice == 2)
+        {
+            int output = Deque(&Q);
+            printf("%d\n", output);
+        }
+    }
+    return 0;
+}
